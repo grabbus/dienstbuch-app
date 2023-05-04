75a7745874f2bcb1de7947e8a0fdd37b29052300
grabbus <mail@michaelgrabinger.com>
2023-05-04 04:36:08 +0200

    added missing migration

---
+++ database/migrations/2023_05_04_023520_update_members_table_add_missing_attribute.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->string('authorized_for_pickup')->after('accepted_data_protection_database')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->dropColumn('authorized_for_pickup');
+        });
+    }
+};
